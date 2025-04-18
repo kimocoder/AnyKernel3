00c6fe0477135386a658680c920398dcced169ae
kimocoder <christian@aircrack-ng.org>
2025-04-18 11:07:49 +0200

    merge changes

--- anykernel.sh
+++ anykernel.sh
@@ -26,13 +26,12 @@ supported.vendorpatchlevels=
 #ramdisk_compression=auto
 #patch_vbmeta_flag=auto
 
-### AnyKernel install
-## boot shell variables
+# boot shell variables
 block=boot
-is_slot_device=auto
+is_slot_device=1
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
-no_magisk_check=1
+#no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
@@ -44,28 +43,27 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 # boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
-else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-fi
+#if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+#    split_boot # for devices with init_boot ramdisk
+#    flash_boot # for devices with init_boot ramdisk
+#else
+#    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+#    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+#fi
 ## end boot install
 
 
 # shell variables
-#block=vendor_boot
-#is_slot_device=1
-#ramdisk_compression=auto
-#patch_vbmeta_flag=auto
+block=vendor_boot
+is_slot_device=1
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
 
 # reset for vendor_boot patching
-#reset_ak
-
+reset_ak
 
 ## AnyKernel vendor_boot install
-#split_boot # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+split_boot # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
 
-#flash_boot
+flash_boot
 ## end vendor_boot install
